d6860ddfb113945ca0d4f4eee94c0ed8f4d4057c
AmrgamalkamelEmam <105132684+AmrgamalkamelEmam@users.noreply.github.com>
2022-05-09 20:58:30 +0200

    Add files via upload

---
+++ js/maldives.js
@@ -0,0 +1,105 @@
+function theme1(checkbox)
+{
+    if(checkbox.checked)
+    {
+    document.getElementById("uu").style.backgroundColor="gray";
+document.getElementById("material-icons").style.color="black";
+
+document.getElementById("p1").style.backgroundColor="gray";
+document.getElementById("p2").style.backgroundColor="gray";
+document.getElementById("p3").style.backgroundColor="gray";
+document.getElementById("p4").style.backgroundColor="gray";
+document.getElementById("p5").style.backgroundColor="gray";
+document.getElementById("p6").style.backgroundColor="gray";
+document.getElementById("p7").style.backgroundColor="gray";
+document.getElementById("p8").style.backgroundColor="gray";
+document.getElementById("p9").style.backgroundColor="gray";
+document.getElementById("p10").style.backgroundColor="gray";
+document.getElementById("p11").style.backgroundColor="gray";
+document.getElementById("p12").style.backgroundColor="gray";
+document.getElementById("p13").style.backgroundColor="gray";
+document.getElementById("p14").style.backgroundColor="gray";
+document.getElementById("h1").style.color="black";
+document.getElementById("footer").style.backgroundColor="gray";
+document.getElementById("ft").style.color="black";
+document.getElementById("si").style.color="black";
+document.getElementById("c1").style.color="black";
+document.getElementById("c1").style.backgroundColor="#9ba3a4";
+document.getElementById("all").style.backgroundColor="#616161";
+document.getElementById("br").style.backgroundColor="#616161";
+document.getElementById("iimmgg").style.backgroundColor="#667d81";
+document.getElementById("imgg").style.backgroundColor="#667d81";
+document.getElementById("bord1").style.borderColor="#380808";
+document.getElementById("bord2").style.borderColor="#380808";
+document.getElementById("vedio").style.borderColor="#380808";
+document.getElementById("bord4").style.borderColor="#380808";
+document.getElementById("bord5").style.borderColor="#380808";
+document.getElementById("iimggg").style.backgroundColor="#667d81";
+document.getElementById("c2").style.color="black";
+document.getElementById("c2").style.backgroundColor="#9ba3a4";
+var c =document.getElementById("contact");
+c.style.backgroundColor="#beb9bb" ;
+var a = document.getElementsByTagName("a");
+for (let i=0 ;i<19 ;i++)
+     {
+
+a[i].style.color="black";
+    }
+
+    }
+
+    else 
+    {
+
+        document.getElementById("uu").style.backgroundColor="aliceblue";
+    document.getElementById("material-icons").style.color="#4e9eff";
+
+    document.getElementById("p1").style.backgroundColor="aliceblue";
+    document.getElementById("p2").style.backgroundColor="aliceblue";
+    document.getElementById("p3").style.backgroundColor="aliceblue";
+    document.getElementById("p4").style.backgroundColor="aliceblue";
+    document.getElementById("p5").style.backgroundColor="aliceblue";
+    document.getElementById("p6").style.backgroundColor="aliceblue";
+    document.getElementById("p7").style.backgroundColor="aliceblue";
+    document.getElementById("p8").style.backgroundColor="aliceblue";
+    document.getElementById("p9").style.backgroundColor="aliceblue";
+    document.getElementById("p10").style.backgroundColor="aliceblue";
+    document.getElementById("p11").style.backgroundColor="aliceblue";
+    document.getElementById("p12").style.backgroundColor="aliceblue";
+    document.getElementById("p13").style.backgroundColor="aliceblue";
+    document.getElementById("p14").style.backgroundColor="aliceblue";
+    document.getElementById("all").style.backgroundColor="#fff";
+    document.getElementById("br").style.backgroundColor="#fff";
+    document.getElementById("iimmgg").style.backgroundColor="#fff";
+    document.getElementById("imgg").style.backgroundColor="#fff";
+    document.getElementById("bord1").style.borderColor="#000";
+    document.getElementById("bord2").style.borderColor="#000";
+    document.getElementById("vedio").style.borderColor="#000";
+    document.getElementById("bord4").style.borderColor="#000";
+    document.getElementById("bord5").style.borderColor="#000";
+    document.getElementById("iimggg").style.backgroundColor="#fff";
+    document.getElementById("ssaa").style.backgroundColor="#fff";
+
+
+
+
+
+
+    var a = document.getElementsByTagName("a");
+    for (let i=0 ;i<19 ;i++){
+        a[i].style.color="#3a6cf4";
+    }
+    document.getElementById("c1").style.backgroundColor="white";
+    document.getElementById("c1").style.color="black";
+    document.getElementById("c2").style.backgroundColor="white";
+    document.getElementById("c2").style.color="black";
+
+    document.getElementById("footer").style.backgroundColor="#24252a";
+    document.getElementById("ft").style.color="#3a6cf4";
+    document.getElementById("si").style.color="#3a6cf4";
+    document.getElementById("hd").style.backgroundColor="aliceblue";
+    document.getElementById("contact").style.backgroundColor="white";
+    document.getElementById("h1").style.color="#3a6cf4";
+    
+    }
+}
